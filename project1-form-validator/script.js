9c1ef4f7b6f93f003b6023266b7c137c4cdcb055
zarathustra90 <radbach@protonmail.com>
2022-03-30 22:39:01 +0100

    script.js if statements created

--- project1-form-validator/script.js
+++ project1-form-validator/script.js
@@ -1,12 +1,27 @@
-const form = document.getElementById('username');
-const username = document.getElementById('email');
-const email = document.getElementById('password');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 // Show input error message
 function showError(input, message){
-    
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//Check if email is valid
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 //Event Listeners
@@ -19,7 +34,27 @@ form.addEventListener('submit', function(e) {
         showError(username, 'Username is required');
     } else {
         showSuccess(username);
-    }
+    } 
+
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else if (!isValidEmail(email.value)) {   
+        showError(email, 'Email is not valid');
+    } else {
+        showSuccess(email);
+    } 
+
+    if (password.value === '') {
+        showError(password, 'password is required');
+    } else {
+        showSuccess(password);
+    } 
+
+    if (password2.value === '') {
+        showError(password2, 'password confirmation is required');
+    } else {
+        showSuccess(password2);
+    } 
     
 });
 
